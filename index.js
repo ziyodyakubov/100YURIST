2cb562e8bc021d6b74bd04c5719b308fe81471d9
ziyodyakubov <ziyodyakubov23@gmail.com>
2024-03-09 17:25:34 +0500

    fixed

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-// MODAL WRAPPER STARTED
-
 let modalWrapper = document.querySelector(".modal-wrapper"),
     open = document.querySelector("#login"),
+    open2 = document.querySelector('#login2'),
     close = document.querySelector("#close"),
-    body = document.querySelector('body');
+    body = document.querySelector('body'),
+    result = document.querySelector('.result-section'),
+    close2 = document.querySelector('#close2'),
+    close3 = document.querySelector('#close3'),
+    showBtn = document.querySelector('#show'),
+    dropdownBtn = document.querySelector('.bx-menu'),
+    dropdownRes = document.querySelector('.dropdown');
 
+
+dropdownBtn.addEventListener('click',()=>{
+    dropdownRes.style.display = 'flex';
+})
+
+
+showBtn.addEventListener('click',()=>{
+    result.style.display = "flex";
+    modalWrapper.style.display = 'none';
+    document.body.style.overflow = "hidden";
+})
+
+
+close2.addEventListener('click',()=>{
+    result.style.display = "none";
+    document.body.style.overflow = "visible";
+})
+
+close3.addEventListener('click',()=>{
+    result.style.display = "none";
+    document.body.style.overflow = "visible";
+})
 
 open.addEventListener("click", () => {
     modalWrapper.style.display = "flex";
     document.body.style.overflow = "hidden";
 })
 
-
-
-modalWrapper.addEventListener('click', (e) => {
-    if (e.target.classList.contains('modal-wrapper-title')) {
-        modalWrapper.style.display = "flex";
-        document.body.style.overflow = "hidden";
-    } else {
-        modalWrapper.style.display = "none";
-        console.log(e.target.classList)
-        document.body.style.overflow = "visible";
-    }
+open2.addEventListener("click", () => {
+    modalWrapper.style.display = "flex";
+    document.body.style.overflow = "hidden";
 })
 
-// MODAL WRAPPER ENDED
+close.addEventListener('click',()=>{
+    modalWrapper.style.display = "none";
+    document.body.style.overflow = "visible";
+})
